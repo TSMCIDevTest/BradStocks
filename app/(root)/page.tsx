1f9d36278a211629e5ff2a6e092a08a2599f9862
CrazyBrad77 <starcodebrad2023@outlook.com>
2026-02-13 12:19:06 -0800

    Added Auth UI and Layout for Auth setup

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -14,7 +14,7 @@ const Home = () => {
             height={600}
           />
         </div>
-        <div className="md:col-span-1 xl:col-span-2">
+        <div className="md:col-span xl:col-span-2">
           <TradingViewWidget 
             title="Stock Heatmap"
             scriptUrl="https://s3.tradingview.com/external-embedding/embed-widget-stock-heatmap.js"
@@ -28,7 +28,6 @@ const Home = () => {
           <TradingViewWidget
             scriptUrl="https://s3.tradingview.com/external-embedding/embed-widget-timeline.js"
             config={TOP_STORIES_WIDGET_CONFIG}
-            className="custom-chart"
             height={600}
           />
         </div>
